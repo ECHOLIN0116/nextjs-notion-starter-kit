d472ab975d631a7d7b6932402392ef8e8cbd9bdb
ECHOLIN0116 <88265395+ECHOLIN0116@users.noreply.github.com>
2023-03-25 00:54:30 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '63a6d5eaf97c4fce82351da98046d62c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '時長．時常',
+  domain: 'https://www.notion.so/63a6d5eaf97c4fce82351da98046d62c',
+  author: 'Echo',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
